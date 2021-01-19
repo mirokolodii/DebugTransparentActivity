3e721094f2b300fd2174c58fac0c8dd2f0a85358
Myroslav Kolodii <myroslav.kolodii@proexe.pl>
2021-01-19 16:13:41 +0100

    First commit

---
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
